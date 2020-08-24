ab035ad337ed5079da1276c06e74b5fafa72b9b4
Deep Bag <43214783+deepbag@users.noreply.github.com>
2020-08-24 14:07:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <!-- Google Adsence -->
     <script data-ad-client="ca-pub-9713082973193234" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- Google Search Console -->
+    <meta name="google-site-verification" content="IO-pm9ZFBU37BeaYlIUCImaWpCA145SYorrDkaAdqk4" />
     <!-- Image animation bootstrap -->
     <style type="text/css">
       .coro img { animation: gocorona 3s linear infinite; }
